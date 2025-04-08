ae28e407816860d98dd4711da67a285926e888c0
fluffygir <naina.talasu@gmail.com>
2025-04-07 20:55:10 -0700

    ll

--- src/simstation/Agent.java
+++ src/simstation/Agent.java
@@ -2,32 +2,73 @@ package simstation;
 
 import java.io.Serializable;
 
-public class Agent implements Runnable, Serializable {
-    public World world;
+abstract public class Agent implements Runnable, Serializable {
     int xc;
     int yc;
-    boolean paused = false;
-    boolean stopped = false;
+    boolean paused, stopped;
     String agentName;
-    Thread myThread;
+    transient protected Thread myThread;
+    World world;
+
+    public Agent(){
+        this.myThread = null;
+        this.agentName = null;
+        this.world = null;
+        this.paused = false;
+        this.stopped = false;
+        this.xc = 0;
+        this.yc = 0;
+    }
+
+    // thread stuff
+    public synchronized void start(){ stopped = false; }
+    public synchronized void stop() { stopped = true; }
+    public synchronized boolean isStopped() { return stopped; }
+    public synchronized void pause(){ paused = true; }
+    public synchronized void resume() { notify(); }
+    public synchronized boolean isPaused() { return paused; }
+    public synchronized void join() {
+        try {
+            if (myThread != null) myThread.join();
+        } catch(InterruptedException e) {
+            System.out.println(e.getMessage()); // changed from default code
+        }
+    }
+
+    //wait for notification if I'm not stopped and I'm suspended
+    private synchronized void checkSuspended() {
+        try {
+            while(!stopped && paused) {
+                wait();
+                paused = false;
+            }
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
     @Override
     public void run() {
-
+        myThread = Thread.currentThread();
+        while (!stopped) {
+            try {
+                onStart();                  //unsure about placement of on___ methods
+                update();
+                onInterrupted();
+                Thread.sleep(1000);
+                checkSuspended();
+                onExit();
+            } catch(InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
-    public void start(){
 
-    }
-    public void stop(){
+    abstract public void update() throws InterruptedException;
 
-    }
-    public void pause(){
+    public void onStart(){ }
 
-    }
-    public void resume(){
+    public void onInterrupted(){ }
 
-    }
-    public void update(){
-
-    }
+    public void onExit(){ }
 }
--- src/simstation/MobileAgent.java
+++ src/simstation/MobileAgent.java
@@ -1,12 +1,50 @@
 package simstation;
 
-public class MobileAgent extends Agent {
+abstract public class MobileAgent extends Agent {
     Heading heading;
-    public void move(int steps){
 
+    public MobileAgent() {
+        super();
+        heading = Heading.EAST;
+    }
+
+    @Override
+    abstract public void update() throws InterruptedException;
+
+    public void move(int steps) throws InterruptedException { //moves agent specified steps in the heading direction
+        if(heading == Heading.EAST) {
+            for (int i = 0; i < steps; i++) {
+                xc += 1;
+                if (xc > world.size) xc -= world.size;
+                world.changed();
+                Thread.sleep(20);
+            }
+        } else if(heading == Heading.WEST){
+            for(int i = 0; i < steps; i++){
+                xc -= 1;
+                if (xc < 0) xc += world.size;
+                world.changed();
+                Thread.sleep(20);
+            }
+        } else if(heading == Heading.NORTH){
+            for(int i = 0; i < steps; i++){
+                yc -= 1;
+                if (yc < 0) yc += world.size;
+                world.changed();
+                Thread.sleep(20);
+            }
+        } else if(heading == Heading.SOUTH){
+            for(int i = 0; i < steps; i++){
+                yc += 1;
+                if (yc > world.size) yc -= world.size;
+                world.changed();
+                Thread.sleep(20);
+            }
+        }
 
     }
-    public void turn(Heading heading){
 
+    public void turn(Heading heading){  //changes heading direction
+        this.heading = heading;
     }
 }
